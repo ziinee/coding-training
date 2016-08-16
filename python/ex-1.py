ab8080ef1c45171cd70cf4764fb771d035b4f59e
Kim Jin Hee <go.jink.kr@gmail.com>
2016-08-16 11:54:38 +0900

    ex-1.py 도전문제 추가

--- python/ex-1.py
+++ python/ex-1.py
@@ -1,4 +1,12 @@
+# 일반문제
+# 이름을 입력받아 인사말을 출력하는 프로그램
+
 name = input("What is your name?")
 msg = "안녕하세요. " + name + "님 반갑습니다."
 print(msg)
+
+
+# 도전문제
+# 1. 변수를 사용하지 않는 버전
+print("안녕하세요. " + input("What is your name?") + "님 반갑습니다.")
 
