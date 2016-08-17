01169a502d971ba37e461561125b60609c813fc2
Kim Jin Hee <go.jink.kr@gmail.com>
2016-08-17 12:00:32 +0900

    ex-2.py 문제풀이
    - GUI 버전 작성은 추후에 재도전

---
+++ python/ex-2.py
@@ -0,0 +1,13 @@
+# 글자 수 세기
+
+length = 0
+text = ''
+
+while (length == 0 ):
+    text = input('What is the input string?')
+    length = len(text) 
+
+print(text + ' has ' + str(length) + ' characters.')
+
+
+
