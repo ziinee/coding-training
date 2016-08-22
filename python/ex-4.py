f817c2bbb5c337f1bd6d15aa9ddbedfc4fd5fd0f
Kim Jin Hee <go.jink.kr@gmail.com>
2016-08-22 13:32:34 +0900

    연습문제 4 풀이

---
+++ python/ex-4.py
@@ -0,0 +1,8 @@
+# Mad Libs 단어를 입력받아, 이야기 문장 만들기
+
+noun = input('Enter a noun: ')
+verb = input('Enter a verb: ')
+adjective = input('Enter an adjective: ')
+adverb = input('Enter an adverb: ')
+
+print("Do you %s your %s %s %s ? That's hilarious!" %(verb, adjective, noun, adverb))
